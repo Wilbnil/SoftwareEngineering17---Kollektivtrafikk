f6ae25466d4e5b817172410aa4343803b9afa864
anna jamissen <annabanasiak468@gmail.com>
2025-09-28 21:22:38 +0200

    created testStops - added 5 stops as an example

---
+++ MainProject/src/main/java/SearchFunction/Stop.java
@@ -0,0 +1,20 @@
+package SearchFunction;
+
+public class Stop {
+    private String name;
+    private int id;
+
+
+    public Stop(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ MainProject/src/main/java/SearchFunction/TestStops.java
@@ -0,0 +1,17 @@
+package SearchFunction;
+
+public class TestStops {
+    public static void main(String[] args) {
+        Stop fredrikstad = new Stop("Fredrikstad", 1);
+        Stop ostfoldhallen = new Stop("Ostfoldhallen", 2);
+        Stop greaaker = new Stop("Greaaker", 3);
+        Stop amfiBorg = new Stop("Amfi Borg", 4);
+        Stop torsbekke = new Stop("Torsbekke", 5);
+
+        System.out.println("Stops created: " + fredrikstad.getName() +  " - " + ostfoldhallen.getName());
+        System.out.println("Stops created: " + greaaker.getName() +  " - " + amfiBorg.getName());
+        System.out.println("Stops created: " + amfiBorg.getName() +  " - " + torsbekke.getName());
+
+    }
+
+}
