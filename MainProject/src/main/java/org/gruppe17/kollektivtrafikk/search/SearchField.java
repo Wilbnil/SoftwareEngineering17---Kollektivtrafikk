6bcd31be486420a339f381ba908fcaa4993e6da4
annab468 <annabanasiak468@gmail.com>
2025-10-04 15:32:29 +0200

    Update SearchField.java
    added comment

--- MainProject/src/main/java/org/gruppe17/kollektivtrafikk/search/SearchField.java
+++ MainProject/src/main/java/org/gruppe17/kollektivtrafikk/search/SearchField.java
@@ -1,5 +1,6 @@
 package org.gruppe17.kollektivtrafikk.search;
 
+//Simulates a search field to provide input for route searches.
 public class SearchField {
     public String getInput() {
         return "Fredrikstad Ostfoldhallen";
