e558a09dfb2eff05779457984a6734d521fc9d49
annab468 <annabanasiak468@gmail.com>
2025-10-04 15:15:36 +0200

    Update RouteService.java
    added comments

--- MainProject/src/main/java/Service/RouteService.java
+++ MainProject/src/main/java/Service/RouteService.java
@@ -6,8 +6,12 @@ import Model.Stop;
 import java.util.Arrays;
 import java.util.List;
 
+//Provides services for retrieving transport routes between stops.
 public class RouteService {
 
+
+    //Retrieves a route between two stops based on their names.
+     * Returns null if no route is found.
     public Route getRoute(String from, String to) {
         if (from.equals("Fredrikstad") && to.equals("Ostfoldhallen")) {
             List<Stop> stops = Arrays.asList(new Stop("Fredrikstad", 1), new Stop("Ostfoldhallen", 2));
@@ -19,7 +23,7 @@ public class RouteService {
             List<Stop> stops = Arrays.asList(new Stop("Torsbekke", 5), new Stop("Fredrikstad", 1));
             return new Route(stops, "bus 33");
         }
-        return null;
+        return null;//Returns null if no route is found.
 
     }
 
