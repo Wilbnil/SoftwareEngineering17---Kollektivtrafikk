fd74ebc71841b07eb7ccb9c6fd6cabd6c3ddf0a9
anna jamissen <annabanasiak468@gmail.com>
2025-10-15 21:22:55 +0200

    test passed for CoordinatesTest and DistanceCalculatorTest

--- MainProject/src/test/java/org/gruppe17/kollektivtrafikk/model/CoordinatesTest.java
+++ MainProject/src/test/java/org/gruppe17/kollektivtrafikk/model/CoordinatesTest.java
@@ -1,4 +1,27 @@
 package org.gruppe17.kollektivtrafikk.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CoordinatesTest {
+
+    @Test
+    public void testDistanceSamePoint() {
+        DistanceCalculator calc = new DistanceCalculator();
+        Coordinates p1 = new Coordinates(0.0, 0.0);
+        Coordinates p2 = new Coordinates(0.0, 0.0);
+        double distance = calc.getDistance(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+        assertEquals(0.0, distance, 0.001, "Distance should be 0 fot he same points");
+    }
+
+
+    @Test
+    public void testDistanceDifferentPoint() {
+        DistanceCalculator calc = new DistanceCalculator();
+        Coordinates p1 = new Coordinates(0.0, 0.0);
+        Coordinates p2 = new Coordinates(3.0, 4.0);
+        double distance = calc.getDistance(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+        assertEquals(5.0, distance, 0.001, "Distance between (0.0) and (3,4) should be 5");
+    }
 }
