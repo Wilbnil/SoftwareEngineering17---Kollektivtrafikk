a3cf11a7f3e41bf6626c248ff7e747e1f9b26de6
Martin Rami Louay Garabet <mrgarabe@hiof.no>
2025-10-15 19:49:32 +0200

    Endret StopComparison etter Coordinates oppdateringen, og nå bruker DistanceCalculator

--- MainProject/src/main/java/org/gruppe17/kollektivtrafikk/service/StopComparison.java
+++ MainProject/src/main/java/org/gruppe17/kollektivtrafikk/service/StopComparison.java
@@ -1,6 +1,7 @@
 package org.gruppe17.kollektivtrafikk.service;
 
 import org.gruppe17.kollektivtrafikk.model.Coordinates;
+import org.gruppe17.kollektivtrafikk.model.DistanceCalculator;
 import org.gruppe17.kollektivtrafikk.model.Stop;
 
 public class StopComparison {
@@ -8,9 +9,15 @@ public class StopComparison {
         Stop closest = null;
         double minsteAvstand = Double.MAX_VALUE;
 
+        DistanceCalculator calculator = new DistanceCalculator();
+
         for (Stop stop : stops) {
             Coordinates stopCoord = new Coordinates(stop.getLongitude(), stop.getLatitude());
-            double distance = searchPoint.distance(stopCoord);
+            double distance = calculator.getDistance(
+                    searchPoint.getX(), searchPoint.getY(),
+                    stopCoord.getX(), stopCoord.getY()
+            );
+
             if (distance < minsteAvstand) {
                 minsteAvstand = distance;
                 closest = stop;
