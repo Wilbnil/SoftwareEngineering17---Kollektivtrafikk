94544ff6352747b284061852e49e1dc0105be9eb
William Nilsen <willian@hiof.no>
2025-10-18 08:53:10 +0200

    Refactored DistanceCalculator and StopComparison  to fit common Utility Logic. modified:   MainProject/.idea/workspace.xml modified:   MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/service/StopComparison.java modified:   MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/utility/DistanceCalculator.java

--- MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/service/StopComparison.java
+++ MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/service/StopComparison.java
@@ -1,7 +1,7 @@
 package gruppe17.kollektivtrafikk.service;
 
+import gruppe17.kollektivtrafikk.utility.DistanceCalculator;
 import org.gruppe17.kollektivtrafikk.model.Coordinates;
-import org.gruppe17.kollektivtrafikk.model.DistanceCalculator;
 import org.gruppe17.kollektivtrafikk.model.Stop;
 
 public class StopComparison {
@@ -9,11 +9,9 @@ public class StopComparison {
         Stop closest = null;
         double minsteAvstand = Double.MAX_VALUE;
 
-        DistanceCalculator calculator = new DistanceCalculator();
-
         for (Stop stop : stops) {
             Coordinates stopCoord = new Coordinates(stop.getLongitude(), stop.getLatitude());
-            double distance = calculator.getDistance(
+            double distance = DistanceCalculator.getDistance(
                     searchPoint.getX(), searchPoint.getY(),
                     stopCoord.getX(), stopCoord.getY()
             );
--- MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/utility/DistanceCalculator.java
+++ MainProject/Model/src/main/java/gruppe17/kollektivtrafikk/utility/DistanceCalculator.java
@@ -1,18 +1,18 @@
-package gruppe17.kollektivtrafikk.model;
+package gruppe17.kollektivtrafikk.utility;
 
 public class DistanceCalculator {
 
-    public DistanceCalculator() {
+    private DistanceCalculator() {
 
     }
 
-    public double getDistance(double fromX, double fromY, double toX, double toY) {
+    public static double getDistance(double fromX, double fromY, double toX, double toY) {
         double dx = toX - fromX;
         double dy = toY - fromY;
         return Math.sqrt(dx * dx + dy * dy);
     }
 
-    public double calculateTravelTime(double distance, double averageSpeedKmH) {
+    public static double calculateTravelTime(double distance, double averageSpeedKmH) {
         return distance / averageSpeedKmH * 60; //time in mins
     }
 
