b51166f3bbb6ac2c829825af8ae1006d53a202bd
Lubb <snorre.spillum@gmail.com>
2025-10-19 11:59:32 +0200

    On item 56, make the getRoutesFromDatabase method and put a little test in main to see if it works

--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/db/DatabaseConnection.java
+++ MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/db/DatabaseConnection.java
@@ -1,38 +1,111 @@
 package org.gruppe17.kollektivtrafikk.db;
 
-import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
 
+import org.gruppe17.kollektivtrafikk.model.Route;
 import org.gruppe17.kollektivtrafikk.model.Stop;
 
 public class DatabaseConnection {
-    // Databaseinformasjon:
+    // Database URL
     public final static String DB_URL = "jdbc:sqlite:MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/db/kollektivdatabase.db";
 
-    public static Stop getRouteFromDatabase(int start) {
+    // Uses a from and to stop from the user and creates a Route object for each route that contains these stops in that order.
+    public static ArrayList<Route> getRoutesFromDatabase(int startStop, int endStop) {
 
-        System.out.println(new File(".").getAbsolutePath());
+        // Checks if startStop is the same as the endStop
+        // The only reason for this is to prevent users breaking the system
+        if (startStop == endStop) {
+            return null;
+        } else {
 
-        // Forbindelse
-        try (Connection connection = DriverManager.getConnection(DB_URL)) {
-            String query = "SELECT * FROM stops WHERE id = ?";
+            // Connection to the database
+            try (Connection connection = DriverManager.getConnection(DB_URL)) {
 
-            PreparedStatement stmnt = connection.prepareStatement(query);
+                // SQL-query for finding info about the Route(s) from the two stops
+                // The "" + "" is only there to make the code more readable
+                String routeQuery =
+                        "SELECT id, name FROM routes AS r " +
+                        "INNER JOIN route_stops AS rs1 ON rs1.route_id = r.id " +
+                        "INNER JOIN route_stops AS rs2 ON rs2.route_id = r.id " +
+                        "WHERE rs2.stop_id = ? " +
+                        "AND rs1.stop_id = ? " +
+                        "AND rs1.stop_order > rs2.stop_order; ";
 
-            stmnt.setInt(1, start);
+                // Turns the query into a prepared statement for execution
+                PreparedStatement routeStatement = connection.prepareStatement(routeQuery);
 
-            ResultSet result = stmnt.executeQuery();
+                // Sets the ? values in the query to the input Stop ids in order
+                // Also serves to protect the database from SQL-injection by checking that the values are integers
+                routeStatement.setInt(1, startStop);
+                routeStatement.setInt(2, endStop);
 
-            while (result.next()) {
-                Stop stop = new Stop(result.getInt("id"), result.getString("name"), result.getString("town"), result.getFloat("longitude"), result.getFloat("latitude"), result.getBoolean("roof"), result.getBoolean("accessibility"));
-                return stop;
+                // Executes the query
+                ResultSet routeResult = routeStatement.executeQuery();
+
+                // Prepares an ArrayList of Routes
+                ArrayList<Route> routeReturn = new ArrayList<Route>();
+
+                // Runs through the resultset and puts the Route objects into the routeReturn ArrayList
+                while (routeResult.next()) {
+                    int id = routeResult.getInt("id");
+                    String name = routeResult.getString("name");
+
+                    routeReturn.add(new Route(id, name));
+                }
+
+
+                // SQL-query to insert Stops into the Route(s)
+                String stopQuery =
+                        "SELECT s.id, s.name, s.town, s.latitude, s.longitude, s.roof, s.accessibility FROM stops AS s " +
+                        "INNER JOIN route_stops AS rs ON s.id = rs.stop_id " +
+                        "WHERE rs.route_id = ? " +
+                        "ORDER BY rs.route_id, rs.stop_order; ";
+
+                // Turns the stopQuery into a prepared statement for execution
+                PreparedStatement stopStatement = connection.prepareStatement(stopQuery);
+
+                // Puts the Stop query in a for loop for each Route we got from the routeQuery
+                for (Route routeX : routeReturn) {
+
+                    // Sets the ? values in the query to the Route ids in order
+                    // Also serves to protect the database from SQL-injection by checking that the values are integers
+                    stopStatement.setInt(1, routeX.getId());
+
+                    // Executes the query
+                    ResultSet stopResult = stopStatement.executeQuery();
+
+                    // Prepares an arraylist of stops
+                    ArrayList<Stop> stops = new ArrayList<Stop>();
+
+                    // Runs through the resultset and puts Stop objects into the stops ArrayList
+                    while (stopResult.next()) {
+                        int id = stopResult.getInt("id");
+                        String name = stopResult.getString("name");
+                        String town = stopResult.getString("town");
+                        float latitude = stopResult.getFloat("latitude");
+                        float longitude = stopResult.getFloat("longitude");
+                        // Converts the int values in the database to boolean (true if not 0, false if 0)
+                        boolean roof = (stopResult.getInt("roof") != 0);
+                        boolean accessibility = (stopResult.getInt("accessibility") != 0);
+
+                        stops.add(new Stop(id, name, town, latitude, longitude, roof, accessibility));
+                    }
+
+                    // Sets the stops ArrayList in the Route objects to the stops ArrayList we got from the query
+                    routeX.setStops(stops);
+                }
+
+                //Returns an ArrayList that contains all the Routes that match the input
+                return routeReturn;
+
+                // Catches exception if the database is unable to connect
+            } catch (SQLException exception) {
+                System.err.println(exception.getMessage());
             }
-
-
-        } catch (SQLException exception) {
-            System.err.println(exception.getMessage());
+            // Returns null if the database is unable to connect
+            return null;
         }
-        return null;
     }
 
 
--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/example/TestStops.java
+++ MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/example/TestStops.java
@@ -1,7 +1,10 @@
 package org.gruppe17.kollektivtrafikk.example;
 
 import org.gruppe17.kollektivtrafikk.db.DatabaseConnection;
+import org.gruppe17.kollektivtrafikk.model.Route;
 import org.gruppe17.kollektivtrafikk.model.Stop;
+
+import java.util.ArrayList;
 
 //Test class to demonstrate the functionality of transport stop and route management.
 
@@ -9,15 +12,26 @@ public class TestStops {
     public static void main(String[] args) {
         DatabaseConnection dbc = new DatabaseConnection();
 
-        Stop test = dbc.getRouteFromDatabase(1);
+        ArrayList<Route> test = dbc.getRoutesFromDatabase(2, 5);
 
-        System.out.println(test.getId());
-        System.out.println(test.getName());
-        System.out.println(test.getTown());
-        System.out.println(test.getLongitude());
-        System.out.println(test.getLatitude());
+        for (Route routeX : test) {
+            System.out.println("Route ---------------");
+            System.out.println("Route ID: " + routeX.getId());
+            System.out.println("Route Name: " + routeX.getName());
+            System.out.println("");
 
-
+            for (Stop stopX : routeX.getStops()) {
+                System.out.println("Stop ---------------");
+                System.out.println("Stop ID: " + stopX.getId());
+                System.out.println("Stop Name: " + stopX.getName());
+                System.out.println("Stop Town: " + stopX.getTown());
+                System.out.println("Stop Latitude: " + stopX.getLatitude());
+                System.out.println("Stop Longitude: " + stopX.getLongitude());
+                System.out.println("Stop Rood: " + stopX.getRoof());
+                System.out.println("Stop Accessibility: " + stopX.getAccessibility());
+                System.out.println("");
+            }
+        }
     }
 }
 
