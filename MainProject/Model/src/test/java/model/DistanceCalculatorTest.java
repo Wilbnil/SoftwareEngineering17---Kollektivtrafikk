86e00837ba37f5086ed638124386cbfe38fcdace
Wilbnil <willian@hiof.no>
2025-10-20 20:02:36 +0200

    On branch 63-refaktorere-DistanceCalculator: Added additional test in DistanceCalculatorTest to deal with less reasonable values.

--- MainProject/Model/src/test/java/model/DistanceCalculatorTest.java
+++ MainProject/Model/src/test/java/model/DistanceCalculatorTest.java
@@ -24,6 +24,7 @@ public class DistanceCalculatorTest {
         // Assert
         assertEquals(LocalTime.of(0, 30), time);
     }
+    // Opplagte testverdier:
     @Test
     public void testDistanceCalculatorDistance_ReasonableValues() {
         //Act
@@ -31,5 +32,28 @@ public class DistanceCalculatorTest {
 
         // Assert
         assertEquals(3, distance);
+    }
+
+    // Mindre forventede verdier:
+    @Test
+    public void testDistanceCalculatorDistance_UnexpectedValues() {
+        //Act
+        double distance1 = DistanceCalculator.getDistance(2.0, 0.0, 2.0, 0.0);
+        double distance2 = DistanceCalculator.getDistance(0.0, 0.0, 0.0, 0.0);
+        double distance3 = DistanceCalculator.getDistance(0.1, 0.1, 0.1, 0.1);
+
+        double distance4 = DistanceCalculator.getDistance(4.0, 3.0, 0.0, 0.0);
+        double distance5 = DistanceCalculator.getDistance(1.0, 10.0, 1.0, 0.0);
+
+        // Assert
+        // Tomme lengder:
+        assertEquals(0, distance1);
+        assertEquals(0, distance2);
+        assertEquals(0, distance3);
+
+        // Negative lengder:
+        assertEquals(5, distance4);
+        assertEquals(10, distance5);
+
     }
 }
