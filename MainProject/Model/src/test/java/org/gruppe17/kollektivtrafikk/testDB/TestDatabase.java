ac8142c3bc8caeb678b724383b980d06c2704cb2
Lubb <snorre.spillum@gmail.com>
2025-11-06 19:43:26 +0100

    Fixed test database to mimic the newly updated database

--- MainProject/Model/src/test/java/org/gruppe17/kollektivtrafikk/testDB/TestDatabase.java
+++ MainProject/Model/src/test/java/org/gruppe17/kollektivtrafikk/testDB/TestDatabase.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import static org.h2.util.ParserUtil.FOREIGN;
+
 public abstract class TestDatabase {
 
     protected Connection connection;
@@ -49,9 +51,30 @@ public abstract class TestDatabase {
                         "route_id INTEGER NOT NULL, " +
                         "stop_id INTEGER NOT NULL, " +
                         "stop_order INTEGER NOT NULL, " +
-                        "PRIMARY KEY (route_id, stop_id)," +
-                        "FOREIGN KEY (route_id) REFERENCES routes(id)," +
-                        "FOREIGN KEY (stop_id) REFERENCES stops(id));");
+                        "PRIMARY KEY(route_id, stop_id), " +
+                        "FOREIGN KEY(route_id) REFERENCES routes(id), " +
+                        "FOREIGN KEY(stop_id) REFERENCES stops(id));");
+
+            // Creates table "timetables"
+            statement.execute(
+                    "CREATE TABLE timetables (" +
+                        "id INTEGER AUTO_INCREMENT, " +
+                        "route_id INTEGER NOT NULL, " +
+                        "day_of_week TEXT NOT NULL, " +
+                        "first_time TEXT NOT NULL, " +
+                        "last_time TEXT NOT NULL, " +
+                        "time_interval INTEGER NOT NULL, " +
+                        "PRIMARY KEY(id), " +
+                        "FOREIGN KEY(route_id) REFERENCES routes(id));");
+
+            // Creates table "route_stop_time"
+            statement.execute(
+                    "CREATE TABLE route_stop_time (" +
+                        "route_id INTEGER, " +
+                        "stop_id INTEGER, " +
+                        "time_from_start INTEGER, " +
+                        "PRIMARY KEY(route_id, stop_id), " +
+                        "FOREIGN KEY(route_id,stop_id) REFERENCES route_stops(route_id,stop_id));");
         }
     }
 
@@ -85,6 +108,36 @@ public abstract class TestDatabase {
             insertIntoRouteStops(2, 3, 4);
             insertIntoRouteStops(2, 2, 5);
             insertIntoRouteStops(2, 1, 6);
+
+            // Inserts data into the "timetables" table
+            insertIntoTimetables(1, "monday", "05:30", "00:25", 10);
+            insertIntoTimetables(1, "tuesday", "05:30", "00:25", 10);
+            insertIntoTimetables(1, "wednesday", "05:30", "00:25", 10);
+            insertIntoTimetables(1, "thursday", "05:30", "00:25", 10);
+            insertIntoTimetables(1, "friday", "05:30", "00:25", 10);
+            insertIntoTimetables(1, "saturday", "07:45", "00:25", 15);
+            insertIntoTimetables(1, "sunday", "09:30", "23:55", 30);
+            insertIntoTimetables(2, "monday", "05:30", "00:25", 10);
+            insertIntoTimetables(2, "tuesday", "05:30", "00:25", 10);
+            insertIntoTimetables( 2, "wednesday", "05:30", "00:25", 10);
+            insertIntoTimetables( 2, "thursday", "05:30", "00:25", 10);
+            insertIntoTimetables( 2, "friday", "05:30", "00:25", 10);
+            insertIntoTimetables( 2, "saturday", "07:45", "00:25", 15);
+            insertIntoTimetables( 2, "sunday", "09:30", "23:55", 30);
+
+            // Inserts data into the "route_stop_time" table
+            insertIntoRouteStopTime(1, 1, 0);
+            insertIntoRouteStopTime(1, 2, 8);
+            insertIntoRouteStopTime(1, 3, 13);
+            insertIntoRouteStopTime(1, 4, 19);
+            insertIntoRouteStopTime(1, 5, 24);
+            insertIntoRouteStopTime(1, 6, 25);
+            insertIntoRouteStopTime(2, 6, 0);
+            insertIntoRouteStopTime(2, 5, 8);
+            insertIntoRouteStopTime(2, 4, 13);
+            insertIntoRouteStopTime(2, 3, 19);
+            insertIntoRouteStopTime(2, 2, 24);
+            insertIntoRouteStopTime(2, 1, 25);
         }
     }
 
@@ -93,6 +146,7 @@ public abstract class TestDatabase {
         try (Statement statement = connection.createStatement()) {
             statement.execute("ALTER TABLE stops ALTER COLUMN id RESTART WITH 1;");
             statement.execute("ALTER TABLE routes ALTER COLUMN id RESTART WITH 1;");
+            statement.execute("ALTER TABLE timetables ALTER COLUMN id RESTART WITH 1;");
         }
     }
 
@@ -150,6 +204,37 @@ public abstract class TestDatabase {
         }
     }
 
+    public void insertIntoTimetables(int route_id, String day_of_week, String first_time, String last_time, int time_interval) throws Exception {
+
+        // Creates an INSERT INTO statement for use
+        String sql = "INSERT INTO timetables (route_id, day_of_week, first_time, last_time, time_interval) " +
+                "VALUES (?, ?, ?, ?, ?)";
+
+        // Places the input parameter values of the method into the statement above
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, route_id);
+            preparedStatement.setString(2, day_of_week);
+            preparedStatement.setString(3, first_time);
+            preparedStatement.setString(4, last_time);
+            preparedStatement.setInt(5, time_interval);
+        }
+    }
+
+    public void insertIntoRouteStopTime(int route_id, int stop_id, int time_from_start) throws Exception {
+
+        // Creates an INSERT INTO statement for use
+        String sql = "INSERT INTO route_stop_time (route_id, stop_id, time_from_start) " +
+                "VALUES (?, ?, ?)";
+
+        // Places the input parameter values of the method into the statement above
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, route_id);
+            preparedStatement.setInt(2, stop_id);
+            preparedStatement.setInt(3, time_from_start);
+        }
+    }
+
+    // Test Methods
     // Serves as a blueprint to count the rows in a table specified by the parameter
     public int countRowsInTable(String tableName) throws Exception {
 
