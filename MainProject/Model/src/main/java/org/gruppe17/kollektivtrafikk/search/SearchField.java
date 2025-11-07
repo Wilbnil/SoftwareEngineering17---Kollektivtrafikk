5156ea84d6984a8fa6a76ac3aec51431406e4783
Lubb <snorre.spillum@gmail.com>
2025-11-07 10:53:04 +0100

    Added some methods in Interface

--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/search/SearchField.java
+++
@@ -1,9 +0,0 @@
-package org.gruppe17.kollektivtrafikk.search;
-
-//Simulates a search field to provide input for route searches.
-public class SearchField {
-    public String getInput() {
-        return "Fredrikstad Ostfoldhallen";
-    }
-
-}
