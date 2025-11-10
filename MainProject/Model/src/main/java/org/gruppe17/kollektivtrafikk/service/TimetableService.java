db2eaf2f7b2c7f8bbb364d18e74401caed4d51c8
PotetStappe1 <ariareply@gmail.com>
2025-11-10 10:57:00 +0100

    On branch notification-service:  created method with logic for sending notification when vehicle is 1 minute away  created empty method for returning the time for the vehicle the user wants a notification from

--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/service/TimetableService.java
+++ MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/service/TimetableService.java
@@ -1,4 +1,36 @@
 package org.gruppe17.kollektivtrafikk.service;
 
+import java.time.*;
+
 public class TimetableService {
+    public LocalTime getBussArrival() {
+        LocalTime today;
+        LocalTime now = LocalTime.now();
+        
+        /*
+         * TODO:
+         * get the bus the user wants a notification for
+         * make sure its the correct day
+         * return the time the bus will arrive to the users current stop
+         */
+
+        return null;
+    }
+
+    public void notification(int stop_id) {
+        LocalTime now = LocalTime.now();
+        LocalTime yourBussArrival = getBussArrival();
+
+        if (yourBussArrival != null) {
+            Duration duration = Duration.between(now, yourBussArrival); 
+            long minutesLeft = duration.toMinutes();
+
+            if (minutesLeft == 1) {
+                System.out.println("Your bus will arrive in 1 minute!");
+            }
+        }
+        else {
+            System.out.println("No bus available.");
+        }
+    }
 }
