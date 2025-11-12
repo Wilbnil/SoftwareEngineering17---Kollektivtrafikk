2aa8bd59e8d14623ceff9cf021acac2063751abe
Wilbnil <willian@hiof.no>
2025-11-12 20:06:47 +0100

    On branch 78-Update-Userdatabase: - Added date variable to store which day the Tour is running on. - Added set-method for routeId.

--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/model/Tour.java
+++ MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/model/Tour.java
@@ -2,6 +2,7 @@ package org.gruppe17.kollektivtrafikk.model;
 
 import org.gruppe17.kollektivtrafikk.utility.DistanceCalculator;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
@@ -10,9 +11,14 @@ public class Tour {
     private int routeId;
     private ArrayList<LocalTime> arrivals;
     private String type;
+    private LocalDate date;
 
     public int getRouteId() {
         return routeId;
+    }
+
+    public void setRouteId(int routeId) {
+        this.routeId = routeId;
     }
 
     public ArrayList<LocalTime> getArrivals() {
@@ -32,4 +38,11 @@ public class Tour {
         return arrivals != null && !arrivals.isEmpty() ? arrivals.get(0) : null;
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
 }
