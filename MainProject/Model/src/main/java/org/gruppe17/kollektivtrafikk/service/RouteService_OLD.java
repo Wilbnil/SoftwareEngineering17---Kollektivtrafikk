852ba6ad92f900aac959b82cc6ce02911f6464a0
Lubb <snorre.spillum@gmail.com>
2025-11-19 18:50:41 +0100

    On branch: develop. Deleted _OLD

--- MainProject/Model/src/main/java/org/gruppe17/kollektivtrafikk/service/RouteService_OLD.java
+++
@@ -1,7 +0,0 @@
-package org.gruppe17.kollektivtrafikk.service;
-
-import org.gruppe17.kollektivtrafikk.model.Route;
-
-public interface RouteService_OLD {
-    Route getRoute(String from, String to);
-}
